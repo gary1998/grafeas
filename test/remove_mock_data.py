cb49b051b8f3da80e9fd5186a68477cda9cd07d4
Alberto Giammaria <agiammar@us.ibm.com>
2018-03-05 19:08:57 -0600

    Changed project id #28

--- test/remove_mock_data.py
+++ test/remove_mock_data.py
@@ -8,7 +8,7 @@ FILE_NAMES = [
     #'mock_data_suspicious_servers.json',
     #'mock_data_suspicious_clients.json',
     #'mock_data_certificates.json',
-    'mock_data_containers_with_vulnerabilities.json',
+    #'mock_data_containers_with_vulnerabilities.json',
     'mock_data_images_with_vulnerabilities.json'
 ]
 

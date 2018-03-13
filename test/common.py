747d4b1d0929947cda9290048d72c44cf5f12c76
Alberto Giammaria <agiammar@us.ibm.com>
2018-03-13 16:43:09 -0500

    App scan tests #30

---
+++ test/common.py
@@ -0,0 +1,147 @@
+from requests import HTTPError
+import json
+import os
+from util import rest_client
+
+
+class CommonTest(object):
+    def __init__(self, api_base_url):
+        self.client = rest_client.RestClient()
+        self.client.add_header("Accept", "application/json")
+        self.client.add_bearer_auth_header(os.environ['IAM_BEARER_TOKEN'])
+        self.api_base_url = api_base_url
+
+    def assert_status(self, response, status_code):
+        if response is None:
+            print("FAILURE")
+        else:
+            print("OK" if response.status_code == status_code else "FAILURE")
+
+    def assert_true(self, value, message):
+            if value:
+                print("OK")
+            else:
+                print("FAILURE: {}".format(message))
+
+    def post(self, url, data, headers=None):
+        try:
+            return self.client.post(url, data, headers)
+        except HTTPError as e:
+            return None
+
+    def get(self, url, headers=None):
+        try:
+            return self.client.get(url, headers)
+        except:
+            return None
+
+    def put(self, url, data, headers=None):
+        try:
+            return self.client.put(url, data, headers)
+        except:
+            return None
+
+    def delete(self, url, headers=None):
+        try:
+            return self.client.delete(url, headers)
+        except:
+            return None
+
+    def post_project(self, body):
+        return self.post(
+            url='{}/v1alpha1/projects'.format(self.api_base_url),
+            data=json.dumps(body),
+            headers={"Content-Type": "application/json"})
+
+    def get_projects(self):
+        return self.get(
+            url='{}/v1alpha1/projects'.format(
+                self.api_base_url))
+
+    def get_project(self, project_id):
+        return self.get(
+            url='{}/v1alpha1/projects/{}'.format(
+                self.api_base_url, project_id))
+
+    def delete_project(self, project_id):
+        return self.delete(
+            url='{}/v1alpha1/projects/{}'.format(
+                self.api_base_url, project_id))
+
+    def post_note(self, project_id, body):
+        return self.post(
+            url='{}/v1alpha1/projects/{}/notes'.format(
+                self.api_base_url, project_id),
+            data=json.dumps(body),
+            headers={"Content-Type": "application/json"})
+
+    def get_notes(self, project_id):
+        return self.get(
+            url='{}/v1alpha1/projects/{}/notes'.format(
+                self.api_base_url, project_id))
+
+    def get_note(self, project_id, note_id):
+        return self.get(
+            url='{}/v1alpha1/projects/{}/notes/{}'.format(
+                self.api_base_url, project_id, note_id))
+
+    def get_occurrence_note(self, project_id, occurrence_id):
+        return self.get(
+            url = '{}/v1alpha1/projects/{}/occurrences/{}/notes'.format(
+                self.api_base_url, project_id, occurrence_id))
+
+    def put_note(self, project_id, note_id, body):
+        return self.put(
+            url='{}/v1alpha1/projects/{}/notes/{}'.format(
+                self.api_base_url, project_id, note_id),
+            data=json.dumps(body),
+            headers={"Content-Type": "application/json"})
+
+    def delete_note(self, project_id, note_id):
+        return self.delete(
+            url='{}/v1alpha1/projects/{}/notes/{}'.format(
+                self.api_base_url, project_id, note_id))
+
+    def post_occurrence(self, project_id, body):
+        return self.post(
+            url='{}/v1alpha1/projects/{}/occurrences'.format(
+                self.api_base_url, project_id),
+            data=json.dumps(body),
+            headers={"Content-Type": "application/json"})
+
+    def post_or_put_occurrence(self, project_id, body):
+        return self.post(
+            url='{}/v1alpha1/projects/{}/occurrences'.format(
+                self.api_base_url, project_id),
+            data=json.dumps(body),
+            headers={
+                "Content-Type": "application/json",
+                "Replace-If-Exists": "true"
+            })
+
+    def get_occurrences(self, project_id):
+        return self.get(
+            url='{}/v1alpha1/projects/{}/occurrences'.format(
+                self.api_base_url, project_id))
+
+    def get_occurrence(self, project_id, occurrence_id):
+        return self.client.get(
+            url='{}/v1alpha1/projects/{}/occurrences/{}'.format(
+                self.api_base_url, project_id, occurrence_id))
+
+    def get_note_occurrences(self, project_id, note_id):
+        return self.get(
+            url='{}/v1alpha1/projects/{}/notes/{}/occurrences'.format(
+                self.api_base_url, project_id, note_id))
+
+    def put_occurrence(self, project_id, occurrence_id, body):
+        return self.put(
+            url='{}/v1alpha1/projects/{}/occurrences/{}'.format(
+                self.api_base_url, project_id, occurrence_id),
+            data=json.dumps(body),
+            headers={"Content-Type": "application/json"})
+
+    def delete_occurrence(self, project_id, occurrence_id):
+        return self.delete(
+            url='{}/v1alpha1/projects/{}/occurrences/{}'.format(
+                self.api_base_url, project_id, occurrence_id))
---
+++ test/dyn_app_scan.py
@@ -0,0 +1,232 @@
+from http import HTTPStatus
+import json
+import os
+from test import common
+
+
+class DynAppScan(common.CommonTest):
+    def __init__(self, api_base_url):
+        super().__init__(api_base_url)
+
+    #
+    #   Projects
+    #
+
+    def test_create_project(self):
+        body = {
+            "id": "project01"
+        }
+
+        response = self.post_project(body)
+        self.assert_status(response, HTTPStatus.OK)
+
+    def test_create_duplicate_project(self):
+        body = {
+            "id": "Project01"
+        }
+
+        response = self.post_project(body)
+        self.assert_status(response, HTTPStatus.CONFLICT)
+
+    def test_get_project(self):
+        response = self.get_project("project01")
+        self.assert_status(response, HTTPStatus.OK)
+
+    def test_list_projects(self):
+        response = self.get_projects()
+        self.assert_status(response, HTTPStatus.OK)
+        results = json.loads(response.content.decode('utf-8'))
+        self.assert_true(len(results) > 0, "An array of one or more projects was expected.")
+
+
+    #
+    #   Notes
+    #
+
+    def test_create_note(self):
+        body = {
+            "id": "Note01",
+            "short_description": "The short description of Note01",
+            "long_description": "The long description of Note01",
+            "kind": "FINDING",
+            "finding": {
+                "severity": "HIGH"
+            }
+        }
+
+        response = self.post_note('project01', body)
+        self.assert_status(response, HTTPStatus.OK)
+
+    def test_create_duplicate_note(self):
+        body = {
+            "id": "Note01",
+            "short_description": "The short description of Note01",
+            "long_description": "The long description of Note01",
+            "kind": "FINDING",
+            "finding": {
+                "severity": "HIGH"
+            }
+        }
+
+        response = self.post_note('project01', body)
+        self.assert_status(response, HTTPStatus.CONFLICT)
+
+    def test_get_note(self):
+        response = self.get_note('project01', 'Note01')
+        self.assert_status(response, HTTPStatus.OK)
+
+    def test_list_notes(self):
+        response = self.get_notes('project01')
+        self.assert_status(response, HTTPStatus.OK)
+        results = json.loads(response.content.decode('utf-8'))
+        self.assert_true(len(results) > 0, "An array of one or more notes was expected.")
+
+    def test_update_note(self):
+        body = {
+            "id": "Note01",
+            "short_description": "The updated short description of Note01",
+            "long_description": "The updated long description of Note01",
+            "kind": "FINDING",
+            "finding": {
+                "severity": "HIGH"
+            }
+        }
+
+        response = self.put_note('project01', 'Note01', body)
+        self.assert_status(response, HTTPStatus.OK)
+
+
+#
+#   Occurrences
+#
+
+    def test_create_occurrence(self):
+        body = {
+            "id": "Occurrence01",
+            "note_name": "projects/project01/notes/Note01",
+            "short_description": "The short description of Occurrence01",
+            "long_description": "The long description of Occurrence01",
+            "kind": "FINDING",
+            "finding": {
+                "certainty": "MEDIUM"
+            },
+            "context": {
+                "account_id": os.environ['RESOURCE_ACCOUNT_ID']
+            }
+        }
+
+        response = self.post_occurrence('project01', body)
+        self.assert_status(response, HTTPStatus.OK)
+
+    def test_create_duplicate_occurrence(self):
+        body = {
+            "id": "Occurrence01",
+            "note_name": "projects/project01/notes/Note01",
+            "short_description": "A different short description of Occurrence01",
+            "long_description": "A different long description of Occurrence01",
+            "kind": "FINDING",
+            "finding": {
+                "certainty": "HIGH"
+            },
+            "context": {
+                "account_id": os.environ['RESOURCE_ACCOUNT_ID']
+            }
+        }
+
+        response = self.post_occurrence('project01', body)
+        self.assert_status(response, HTTPStatus.CONFLICT)
+
+    def test_create_or_update_occurrence(self):
+        body = {
+            "id": "Occurrence01",
+            "note_name": "projects/project01/notes/Note01",
+            "short_description": "Updated short description of Occurrence01",
+            "long_description": "Updated long description of Occurrence01",
+            "kind": "FINDING",
+            "finding": {
+                "certainty": "HIGH"
+            },
+            "context": {
+                "account_id": os.environ['RESOURCE_ACCOUNT_ID']
+            }
+        }
+
+        response = self.post_or_put_occurrence('project01', body)
+        self.assert_status(response, HTTPStatus.OK)
+
+    def test_get_occurrence(self):
+        response = self.get_occurrence('project01', 'Occurrence01')
+        self.assert_status(response, HTTPStatus.OK)
+
+    def test_get_occurrence_note(self):
+        response = self.get_occurrence_note('project01', 'Occurrence01')
+        self.assert_status(response, HTTPStatus.OK)
+
+    def test_list_occurrences(self):
+        response = self.get_occurrences('project01')
+        self.assert_status(response, HTTPStatus.OK)
+        results = json.loads(response.content.decode('utf-8'))
+        self.assert_true(len(results) > 0, "A array of one ore more occurrences was expected.")
+
+    def test_list_note_occurrences(self):
+        response = self.get_note_occurrences('project01', 'Note01')
+        self.assert_status(response, HTTPStatus.OK)
+        results = json.loads(response.content.decode('utf-8'))
+        self.assert_true(len(results) > 0, "An array of one or more occurrences was expected.")
+
+
+
+    #
+    # Cleanup
+    #
+
+    def test_delete_occurrence(self):
+        response = self.delete_occurrence('project01', 'Occurrence01')
+        self.assert_status(response, HTTPStatus.OK)
+
+    def test_delete_missing_occurrence(self):
+        response = self.delete_occurrence('project01', 'Occurrence01')
+        self.assert_status(response, HTTPStatus.NOT_FOUND)
+
+    def test_delete_note(self):
+        response = self.delete_note('project01', 'Note01')
+        self.assert_status(response, HTTPStatus.OK)
+
+    def test_delete_missing_note(self):
+        response = self.delete_note('project01', 'Note01')
+        self.assert_status(response, HTTPStatus.NOT_FOUND)
+
+    def test_delete_project(self):
+        response = self.delete_project("project01")
+        self.assert_status(response, HTTPStatus.OK)
+
+    def test_delete_missing_project(self):
+        response = self.delete_project("project01")
+        self.assert_status(response, HTTPStatus.NOT_FOUND)
+
+
+if __name__ == '__main__':
+    test = DynAppScan(api_base_url="https://grafeas.ng.bluemix.net")
+    test.test_create_project()
+    test.test_create_duplicate_project()
+    test.test_get_project()
+    test.test_list_projects()
+    test.test_create_note()
+    test.test_create_duplicate_note()
+    test.test_get_note()
+    test.test_list_notes()
+    test.test_update_note()
+    test.test_create_occurrence()
+    test.test_create_duplicate_occurrence()
+    test.test_create_or_update_occurrence()
+    test.test_get_occurrence()
+    test.test_get_occurrence_note()
+    test.test_list_occurrences()
+    test.test_list_note_occurrences()
+    test.test_delete_occurrence()
+    test.test_delete_missing_occurrence()
+    test.test_delete_note()
+    test.test_delete_missing_note()
+    test.test_delete_project()
+    test.test_delete_missing_project()
+

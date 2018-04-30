93b466206edbd85ba7595ae0e25c02abadd1fea9
Jagdish Kumar <jagkuma3@in.ibm.com>
2018-04-30 19:28:36 +0530

    Fix typo in ssl environment variable

--- app.py
+++ app.py
@@ -23,16 +23,17 @@ if __name__ == '__main__':
     grafeas_configure_ssl = os.environ.get('GRFAEAS_CONFIGURE_SSL', "false")
     if grafeas_configure_ssl.lower() == 'true':
         cert_path = os.environ.get('SSL_CERT_PATH')
-        cert_key = os.environ.get('SSL_CERT_KEY')
+        cert_key = os.environ.get('SSL_KEY_PATH')
 
         if cert_path is None:
             logger.error("GRFAEAS_CONFIGURE_SSL is set to true but SSL_CERT_PATH is not set. ")
             sys.exit(1)
         if cert_key is None:
-            logger.error("GRFAEAS_CONFIGURE_SSL is set to true but SSL_CERT_KEY is not set.")
+            logger.error("GRFAEAS_CONFIGURE_SSL is set to true but SSL_KEY_PATH is not set.")
             sys.exit(1)
 
         context = (cert_path, cert_key)
         app.run(port=8080, threaded=True, ssl_context=context)
     else:
         app.run(port=8080, threaded=True)
+

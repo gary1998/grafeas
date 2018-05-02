5b8fbde35bffe2351575504f4f0f6f8d6d325e3a
Jagdish Kumar <jagkuma3@in.ibm.com>
2018-05-02 16:07:31 +0530

    Fix typo in grafeas ssl config environment variable

--- app.py
+++ app.py
@@ -20,16 +20,16 @@ logger.addHandler(stream_handler)
 if __name__ == '__main__':
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.add_api('swagger.yaml', arguments={'title': 'IBM Grafeas API'})
-    grafeas_configure_ssl = os.environ.get('GRFAEAS_CONFIGURE_SSL', "false")
+    grafeas_configure_ssl = os.environ.get('GRAFEAS_CONFIGURE_SSL', "false")
     if grafeas_configure_ssl.lower() == 'true':
         cert_path = os.environ.get('SSL_CERT_PATH')
         cert_key = os.environ.get('SSL_KEY_PATH')
 
         if cert_path is None:
-            logger.error("GRFAEAS_CONFIGURE_SSL is set to true but SSL_CERT_PATH is not set. ")
+            logger.error("GRAFEAS_CONFIGURE_SSL is set to true but SSL_CERT_PATH is not set. ")
             sys.exit(1)
         if cert_key is None:
-            logger.error("GRFAEAS_CONFIGURE_SSL is set to true but SSL_KEY_PATH is not set.")
+            logger.error("GRAFEAS_CONFIGURE_SSL is set to true but SSL_KEY_PATH is not set.")
             sys.exit(1)
 
         context = (cert_path, cert_key)
